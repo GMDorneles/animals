66b0c08c33eb60b9ed24e485bde0e17657ce8c03
Gabrielly Maldonado <gabrielly.maldonadoo@gmail.com>
2024-06-16 15:06:59 -0300

    feat: api get

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,15 @@
-export async function getAnimals() {
-  const res = await fetch("http://localhost:3000/api/animals");
-  if (!res.ok) {
-    throw new Error("Failed to fetch animals.");
+import { Animal } from "@/types/animal/animal";
+
+export async function getAnimals(): Promise<Animal[]> {
+  try {
+    const res = await fetch("http://localhost:3001/animals");
+    if (!res.ok) {
+      throw new Error("Erro ao buscar");
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Erro ao buscar os dados dos animais:", error);
+    throw error;
   }
-  return await res.json();
 }
